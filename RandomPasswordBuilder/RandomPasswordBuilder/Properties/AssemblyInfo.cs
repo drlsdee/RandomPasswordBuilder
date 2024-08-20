bda12c628f46f9ef50f0674ea952d8903bc36086
ilinyh_s <ilinyh_s@goldapple.ru>
2024-08-20 18:50:04 +0500

    Meta: assembly description added

--- RandomPasswordBuilder/RandomPasswordBuilder/Properties/AssemblyInfo.cs
+++ RandomPasswordBuilder/RandomPasswordBuilder/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RandomPasswordBuilder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is just another .NET library generating random strings that are guaranteed to include all specified character categories.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RandomPasswordBuilder")]
